7e69913b313dbb7af16577e3d2205ae4a6ffc719
1omarsaid <omar.said@ryerson.ca>
2020-09-26 19:16:35 -0400

    initial Commit

---
+++ src/components/social.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { socialMedia } from '@config';
+import { Side } from '@components';
+import { Icon } from '@components/icons';
+
+const StyledSocialList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 0px;
+  list-style: none;
+
+
+  li {
+    padding: 10px;
+
+    &:last-of-type {
+      margin-bottom: 20px;
+    }
+
+    a {
+      &:hover,
+      &:focus {
+        transform: translateY(-3px);
+      }
+
+      svg {
+        width: 18px;
+        height: 18px;
+      }
+    }
+  }
+`;
+
+const Social = ({ isHome }) => (
+  <Side isHome={isHome} orientation="left">
+    <StyledSocialList>
+      {socialMedia &&
+        socialMedia.map(({ url, name }, i) => (
+          <li key={i}>
+            <a target="_blank" href={url} aria-label={name}>
+              <Icon name={name} />
+            </a>
+          </li>
+        ))}
+    </StyledSocialList>
+  </Side>
+);
+
+Social.propTypes = {
+  isHome: PropTypes.bool,
+};
+
+export default Social;
